0c07294c24fd3e65deeefa3bfb296ad14f7bc982
kishansudani <48625369+kishansudani@users.noreply.github.com>
2022-11-11 22:12:23 +0530

    discord bot coin

---
+++ Discord/Coin/README.md
@@ -0,0 +1,17 @@
+
+- Enter the below details in `.env.` file which match with your coin.conf file
+```bash
+TOKEN=""
+RPCURL=
+RPCPORT=
+RPCUSER=""
+RPCPASSWORD=""
+```
+
+```bash
+apt install python3-dotenv
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ Discord/Coin/main.py
@@ -0,0 +1,34 @@
+import discord
+import os
+from dotenv import load_dotenv
+# from Discord.Coin.keep_alive import keep_alive
+# import Discord.Coin.rpc as rpc
+# from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+load_dotenv()
+
+
+# RPCUSER = os.getenv('RPCUSER')
+# RPCPASSWORD = os.getenv('RPCPASSWORD')
+# RPCPORT = os.getenv('RPCPORT')
+
+# rpc_connection = AuthServiceProxy(f"http://{RPCUSER}:{RPCPASSWORD}@127.0.0.1:{RPCPORT}")
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print("Bot is now ready to use!!!")
+    print("--------------------------")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    msg = message.content
+
+    if msg.startswith('$qoute'):
+        await message.channel.send("Hiii!!!!!!!")
+
+client.run(os.getenv('TOKEN'))
